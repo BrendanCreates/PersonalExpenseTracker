c94007d5e9567cd8485c0bd85a371be2b47360c2
BrendanCreates <bnevilsstem2023@gmail.com>
2025-04-05 19:02:12 -0400

    Working on CSV data

--- PersonalExpenseManager.py
+++ PersonalExpenseManager.py
@@ -11,16 +11,13 @@ Features:
 License: MIT License
 
 """
-import pandas as pd
+import csv
 
 class manager:
     """ Expense Manager Constructor """
     def __init__(self):
-        self.expenses = {} # category : amount, description
-        self.data = [
-            ["Category", "Amount", "Description"],
-        ]
-        df = pd.DataFrame(self.data[1:], columns=self.data[0])    
+        self.expenses = {} # category : [[amount, description], ... ]
+    
     """ --------------------------- """       
 
 
@@ -34,24 +31,20 @@ class manager:
     def print_all_categories(self):
         categories = self.get_all_categories(self)
         print(", ".join(categories)) # prints the elements of categories with the the string in between
+    
     """ ----------------- """
 
 
     """ Expense Functions """
     def add_expense(self, amount, category, desc):
-        try:
-            amount = float(amount)
-        except:
-            return False
         if ([amount, desc] in self.expenses[category]):
             return False
         if (category in self.expenses):
             self.expenses[category].append([amount, desc])
-            self.data.append([category, str(amount), desc])
             return True
         else:
             self.expenses[category] = [[amount, desc]]
-            self.data.append([category, str(amount), desc])
+            return True
 
     def get_all_expenses_by_category(self, category):
         result = []
@@ -75,36 +68,31 @@ class manager:
         for expense in self.expenses.values():
             total += expense[0]
         return total
+    
     """ ----------------- """
 
 
-    """ General Functions """
-    # def sync_data_expenses(self):
-    def write_to_file(self, file_name):
-        with open(file_name, "w") as file:
+    """ File Functions """
+    def write_csv(self):
+        with open("Expenses.csv", "w", newline="") as file: # newline="" forces to handle newlines normally not Windows way with carriage return
+            writer = csv.writer(file)
+            
+            csv.writerow(["Category", "Amount", "Description"])
+
             for key, value in self.expenses.items():
-                file.write(key + ", " + str(value[0]) + " " + )
+                for expense in value:
+                    writer.writerow([key, expense[0], expense[1]])
+            return True
+        
+    def read_csv(self):
+        with open("Expenses.csv", "r", newline="") as file:
+            reader = csv.DictReader(file)
+            
+            for row in reader:
+                category = row["Category"]
             
 
-        try: 
-            amount = input("What is expense cost?: ")
-            amount = float(amount) 
-        except ValueError:
-            print("Error: Please enter a valid number")
-            return
-        
-        try: 
-            
-            category = input("Enter an amount: ")
-            amount = float(amount) 
-        except ValueError:
-            print("Error: Please enter a valid number")
-            return
 
-        try: 
-            amount = input("Enter an amount: ")
-            amount = float(amount) 
-        except ValueError:
-            print("Error: Please enter a valid number")
-            return
-        
+    
+
+
